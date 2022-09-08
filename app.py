bf7f5215ab5c983b6c522ba8abd2f6ac1531b459
user <brezent12345@gmail.com>
2022-09-08 15:33:30 +0300

    с

--- app.py
+++ app.py
@@ -4,6 +4,19 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    category = db.Column(db.String(100), nullable=False)
+    isActive = db.Column(db.Boolean, default=True)
+
+    def __repr__(self):
+        return '<Item %r>' % self.id
+
 
 
 @app.route('/')
