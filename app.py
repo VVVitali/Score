d8524948c629f2b428f4d1009cf2484fb3c81ad3
user <brezent12345@gmail.com>
2022-09-11 17:36:09 +0300

    e

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -21,16 +21,34 @@ class Item(db.Model):
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    items = Item.query.order_by(Item.price).all()
+    return render_template('index.html', data=items)
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
 
-@app.route('/about')
+@app.route('/create', methods=['POST', 'GET'])
 def create():
-    return render_template('create.html')
+    if request.method == 'POST':
+        title = request.form['title']
+        price = request.form['price']
+        category = request.form['category']
+
+        item = Item(title=title, price=price, category=category)
+
+        try:
+            db.session.add(item)
+            db.session.commit()
+            return redirect('/')
+
+        except:
+            return "Произошла ошибка"
+
+    else:
+        return render_template('create.html')
+
 
 
 if __name__ == '__main__':
